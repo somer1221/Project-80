174c38eb64ddf92b8eca421d8f729def4c9e7663
somer1221 <79031075+somer1221@users.noreply.github.com>
2021-05-09 12:54:06 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var list_of_guests = [];
+
+function addguest() {
+var guest_name = document.getElementById("name").value;
+list_of_guests.push(guest_name);
+document.getElementById("displaynovertical").innerHTML = list_of_guests;
+console.log(list_of_guests);
+}
+function sortguests(); {
+namesofpeople.sort();
+}
+
+function searching(); {
+
+    var s = document.getElementById("s1")
+}
